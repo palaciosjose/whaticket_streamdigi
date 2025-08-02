f0fcc905e3b429212c02e910d413248ea9371782
palaciosjose <141429853+palaciosjose@users.noreply.github.com>
2025-08-02 22:04:37 +0100

    feat: rename spanish translation

--- frontend/src/translate/languages/index.js
+++ frontend/src/translate/languages/index.js
@@ -1,11 +1,9 @@
-import { messages as portugueseMessages } from "./pt";
 import { messages as englishMessages } from "./en";
-import { messages as spanishMessages } from "./es";
+import { messages as spanishMessages } from "../../pages/es";
 
 const messages = {
-	...portugueseMessages,
-	...englishMessages,
-	...spanishMessages,
+        ...englishMessages,
+        ...spanishMessages,
 };
 
 export { messages };
